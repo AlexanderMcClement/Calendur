59715664b5acfd5ca2b9ada9e26a3d458d922888
Alexander <alexandermcclement9@gmail.com>
2020-01-04 16:01:37 -0800

    update2

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 $(document).ready(function(){
-console.log("script is link")
+console.log("script is link");
+console.log(moment().format());
+console.log(moment().format('YYYY MM DD')
+);
+$('#date').text(moment().format('YYYY MM DD'))
+console.log(moment().hours())
+});
 
-})
