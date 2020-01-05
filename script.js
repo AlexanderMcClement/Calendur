e93110d5c7d547ecf1cd2d658e1b46e7b4f6ec3e
Alexander <alexandermcclement9@gmail.com>
2020-01-04 20:35:17 -0800

    update4

--- script.js
+++ script.js
@@ -1,15 +1,37 @@
-$(document).ready(function(){
-console.log("script is link");
-console.log(moment().format());
-console.log(moment().format('YYYY MM DD')
-);
-$('#date').text(moment().format('YYYY MM DD'))
-console.log(moment().hours())
+$(document).ready(function() {
+  $("#date").text(moment().format("YYYY MM DD"));
+  console.log(moment().hours());
 
-$('button').on("click" , function(){
-    console.log($(this).parent().siblings('.inputdiv').children().val())
+  $("button").on("click", function() {
+    var inputvalue = $(this)
+      .parent()
+      .siblings(".inputdiv")
+      .children()
+      .val();
 
-})
+    var key = $(this)
+      .parent()
+      .siblings(".inputdiv")
+      .children()
+      .attr("id");
+
+    
+
+    localStorage.setItem(key, inputvalue);
+  });
+
+  $("#7").val(localStorage.getItem("7"))
+  $("#8").val(localStorage.getItem("8"))
+  $("#9").val(localStorage.getItem("9"))
+  $("#10").val(localStorage.getItem("10"))
+  $("#11").val(localStorage.getItem("11"))
+  $("#12").val(localStorage.getItem("12"))
+  $("#13").val(localStorage.getItem("13"))
+  $("#14").val(localStorage.getItem("14"))
+  $("#15").val(localStorage.getItem("15"))
+  $("#16").val(localStorage.getItem("16"))
+  $("#17").val(localStorage.getItem("17"))
+
+  
 
 });
-
