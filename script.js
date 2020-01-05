6fac97fb356bbe7f1eea941df6a97b134d3b22b0
Alexander <alexandermcclement9@gmail.com>
2020-01-04 16:30:08 -0800

    update3

--- script.js
+++ script.js
@@ -5,5 +5,11 @@ console.log(moment().format('YYYY MM DD')
 );
 $('#date').text(moment().format('YYYY MM DD'))
 console.log(moment().hours())
+
+$('button').on("click" , function(){
+    console.log($(this).parent().siblings('.inputdiv').children().val())
+
+})
+
 });
 
